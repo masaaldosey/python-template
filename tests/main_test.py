e051a62ba619037a7b89a5eb36d199efdba5028e
masaaldosey <prabhatreddyk@gmail.com>
2022-12-11 18:09:45 +0100

    add corresponding tests

---
+++ tests/main_test.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from python_template.product import main
+
+def test_main_trivial():
+    assert main((["--op1=6", "--op2=6"])) == 1
+    assert main((["--op1=6", "--op2=t"])) == 0
---
+++ tests/multiply_two_nums_test.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from python_template.product import multiply_two_nums
+
+def test_multiplication():
+    assert multiply_two_nums(3, 3) == 9
+    assert multiply_two_nums(0, 0) == 0
+    assert multiply_two_nums(-1, 6) == -6
+    assert multiply_two_nums(-1, -1) == 1
